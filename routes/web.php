f9d0bccfc4461ead96d693fe1e327f75931e89d2
Evgeniy Mikhalev <kopperphild@gmail.com>
2019-11-25 15:06:40 +0500

    auth

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/nomenclature', 'NomenclatureController@index');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
