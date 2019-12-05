476e475f388d68b3a8dcfbf2d833e7484e870b09
Evgeniy Mikhalev <kopperphild@gmail.com>
2019-12-05 18:57:23 +0500

    added CostItem seeder

---
+++ database/seeds/CostItemSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\CostItem;
+
+class CostItemSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        CostItem::firstOrCreate([
+            'id' => 1,
+            'name' => 'Прочее'
+        ]);
+
+        CostItem::firstOrCreate([
+            'id' => 2,
+            'name' => 'Дом'
+        ]);
+
+        CostItem::firstOrCreate([
+            'id' => 3,
+            'name' => 'Развлечения'
+        ]);
+
+        CostItem::firstOrCreate([
+            'id' => 4,
+            'name' => 'Транспорт'
+        ]);
+
+        CostItem::firstOrCreate([
+            'id' => 5,
+            'name' => 'Спорт'
+        ]);
+    }
+}
