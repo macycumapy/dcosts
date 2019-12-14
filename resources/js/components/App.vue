0df569b3e54f67da54068b0e5e1e9fc53cc4c949
Evgeniy Mikhalev <kopperphild@gmail.com>
2019-12-14 18:41:00 +0500

    added new routes

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,23 +1,32 @@
 <template>
     <div>
-        <div v-if="this.$store.getters.isAuthenticated">
-            <h1>Daily costs</h1>
-            <ul>
-                <li>
-                    <router-link :to="{ name: 'home' }">Home</router-link>
-                </li>
-                <li>
-                    <router-link :to="{ name: 'users' }">Users</router-link>
-                </li>
-                <li><a href="javascript:void(0);" @click="logout">Logout</a></li>
-            </ul>
-            <div class="container">
+        <div v-if="this.$store.getters.isAuthenticated" id="lk">
+            <div id="lk-menu" class="py-3">
+                <div class="row">
+                    <div class="col-12">
+                        <router-link :to="{ name: 'home' }">
+                            <div class="logo m-auto"></div>
+                        </router-link>
+                    </div>
+                </div>
+                <ul class="py-4 pl-1">
+                    <li><router-link :to="{ name: 'nomenclature' }">Номенклатура</router-link></li>
+                    <li><router-link :to="{ name: 'nomenclatureType' }">Номенклатурные группы</router-link></li>
+                    <li><router-link :to="{ name: 'costItems' }">Статьи затрат</router-link></li>
+                </ul>
+                <ul class="py-1 pl-1">
+                    <li><a href="javascript:void(0);" @click="logout">Выйти</a></li>
+                </ul>
+            </div>
+            <div class="container-fluid py-3">
                 <router-view></router-view>
             </div>
         </div>
         <div v-else id="enter-system">
             <div class="header row">
-                <div class="left"><a href="/"><div class="logo"></div></a></div>
+                <div class="left"><a href="/">
+                    <div class="logo"></div>
+                </a></div>
                 <div class="right">
                     <router-link :to="{ name: 'register' }">Регистрация</router-link>
                     <router-link :to="{ name: 'login' }">Вход</router-link>
@@ -28,7 +37,8 @@
                     <router-view></router-view>
                 </div>
             </div>
-            <div class="footer">{{ new Date().getFullYear() }} &copy; DailyCosts <a href="mailto:kopperphild@gmail.com">kopperphild@gmail.com</a></div>
+            <div class="footer">{{ new Date().getFullYear() }} &copy; DailyCosts <a href="mailto:kopperphild@gmail.com">kopperphild@gmail.com</a>
+            </div>
         </div>
     </div>
 </template>
--- resources/js/router.js
+++ resources/js/router.js
@@ -2,8 +2,27 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import {store} from './vuex/store.js'
 
+
+const onlyAuthenticated = (to, from, next) => {
+    if (store.getters.isAuthenticated) {
+        next();
+        return
+    }
+    next('/login')
+};
+
+const onlyNotAuthenticated = (to, from, next) => {
+    if (!store.getters.isAuthenticated) {
+        next();
+        return
+    }
+    next('/')
+};
+
 import Home from './components/Home'
-import Users from './components/Users'
+import CostItems from './components/Dictionaries/CostItem'
+import Nomenclature from './components/Dictionaries/Nomenclature'
+import NomenclatureType from './components/Dictionaries/NomenclatureType'
 import Login from './components/Auth/Login'
 import Register from './components/Auth/Register'
 
@@ -16,31 +35,37 @@ export const router = new Router({
             path: '/',
             name: 'home',
             component: Home,
+            beforeEnter: onlyAuthenticated,
         },
         {
             path: '/login',
             name: 'login',
             component: Login,
+            beforeEnter: onlyNotAuthenticated,
         },
         {
             path: '/register',
             name: 'register',
             component: Register,
+            beforeEnter: onlyNotAuthenticated,
         },
         {
             path: '/cost_items',
-            name: 'users',
-            component: Users,
+            name: 'costItems',
+            component: CostItems,
+            beforeEnter: onlyAuthenticated,
+        },
+        {
+            path: '/nomenclature',
+            name: 'nomenclature',
+            component: Nomenclature,
+            beforeEnter: onlyAuthenticated,
+        },
+        {
+            path: '/nomenclature_type',
+            name: 'nomenclatureType',
+            component: NomenclatureType,
+            beforeEnter: onlyAuthenticated,
         },
     ],
-});
-
-router.beforeEach((to, from, next) => {
-    if (store.getters.isAuthenticated) {
-        if (to.name === 'login' || to.name === 'register') next({name: 'home'});
-        else next()
-    } else {
-        if (to.name === 'login' || to.name === 'register') next();
-        else next({name: 'login'})
-    }
 });
