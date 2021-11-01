8bc6f288366cc17b1200ac0dc815034209f63139
macycumapy <kopperphild@gmail.com>
2021-11-01 13:09:11 +0500

    Add: базовые ответы

---
+++ app/Helpers/Global/response_functions.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Http\JsonResponse;
+
+if (!function_exists('make_response')) {
+    /**
+     * JSON ответ на API запрос
+     *
+     * @param int $status Статус ответа
+     * @param string $message Сообщение о результате обращения
+     * @param mixed $data Данные полученные в результате обращения
+     * @param array|null $errors Список ошибок, возникших во время обращения
+     *
+     * @return JsonResponse
+     */
+    function make_response(string $message = '', int $status = 200, $data = null, array $errors = null): JsonResponse
+    {
+        $responseData = [
+            'message' => $message
+        ];
+
+        /**
+         * Добавляем блок данных только при их наличии
+         */
+        if (!empty($data)) {
+            $responseData['data'] = $data;
+        }
+
+        /**
+         * Добавляем блок ошибок только при их наличии
+         */
+        if (!empty($errors)) {
+            $responseData['errors'] = $errors;
+        }
+
+        return response()->json(
+            $responseData,
+            $status,
+            [
+                'Content-Type' => 'application/json;charset=UTF-8',
+                'Charset' => 'utf-8'
+            ],
+            JSON_UNESCAPED_UNICODE
+        );
+    }
+}
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,64 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Execute an action on the controller.
+     *
+     * @param string $method
+     * @param array  $parameters
+     *
+     * @return Response
+     */
+    public function callAction($method, $parameters)
+    {
+        try {
+            return parent::callAction($method, $parameters);
+        } catch (\Throwable $exception) {
+            Log::error($exception);
+            return make_response(500, 'Возникла внутренняя ошибка!');
+        }
+    }
+
+    /**
+     * @param string $message
+     * @param null $data
+     * @return JsonResponse
+     */
+    protected function successResponse(string $message = '', $data = null): JsonResponse
+    {
+        return make_response($message, 200, $data);
+    }
+
+    /**
+     * @param string $message
+     * @param null   $data
+     * @param null   $errors
+     *
+     * @return JsonResponse
+     */
+    protected function failureResponse(string $message = '', $errors = null, $data = null): JsonResponse
+    {
+        return make_response($message, 400, $data, $errors);
+    }
+
+    /**
+     * @param string $message
+     * @param null   $data
+     * @param null   $errors
+     *
+     * @return JsonResponse
+     */
+    protected function errorResponse(string $message = '', $errors = null, $data = null): JsonResponse
+    {
+        return make_response($message, 500, $data, $errors);
+    }
 }
