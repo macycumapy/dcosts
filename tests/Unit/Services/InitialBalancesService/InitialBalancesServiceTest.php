59e4a0f99802e783c4eb06acf0440b997c309e4d
macycumapy <kopperphild@gmail.com>
2023-02-08 13:04:47 +0500

    Загрузка поступлений через xlsx

--- tests/Unit/Services/InitialBalancesService/InitialBalancesServiceTest.php
+++ tests/Unit/Services/InitialBalancesService/InitialBalancesServiceTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Services\InitialBalancesService;
 
+use App\Enums\CashFlowType;
 use App\Models\CashFlow;
 use App\Models\CostItem;
 use App\Models\Partner;
@@ -12,6 +13,7 @@ use App\Services\InitialBalancesService\DTO\InflowDTO;
 use App\Services\InitialBalancesService\InflowsXlsxParser;
 use App\Services\InitialBalancesService\InitialBalancesService;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 use Tests\Traits\TestStorage;
 
@@ -38,18 +40,31 @@ class InitialBalancesServiceTest extends TestCase
         $data = (new InflowsXlsxParser)->parse($file);
 
         $this->assertEquals($data->count(), $result->count());
-        /** @var InflowDTO $inflow */
         foreach ($data as $inflow) {
-            $partner = Partner::query()->firstWhere('name', $inflow->partnerName);
-            $costItem = CostItem::query()->firstWhere('name', $inflow->costItemName);
-            $this->assertModelExists($partner);
-            $this->assertModelExists($costItem);
-            $this->assertTrue(CashFlow::query()->where([
-                'date' => $inflow->date,
-                'sum' => $inflow->sum,
-                'partner_id' => $partner->id,
-                'cost_item_id' => $costItem->id,
-            ])->exists());
+            $this->assertInflowsCreated($inflow);
         }
+    }
+
+    private function assertInflowsCreated(InflowDTO $inflow)
+    {
+        $partner = Partner::query()->where([
+            'name' => $inflow->partnerName,
+            'user_id' => Auth::id(),
+        ])->first();
+        $costItem = CostItem::query()->where([
+            'name' => $inflow->costItemName,
+            'type' => CashFlowType::Inflow,
+            'user_id' => Auth::id(),
+        ])->first();
+        $this->assertModelExists($partner);
+        $this->assertModelExists($costItem);
+        $this->assertTrue(CashFlow::query()->where([
+            'type' => CashFlowType::Inflow,
+            'user_id' => Auth::id(),
+            'date' => $inflow->date,
+            'sum' => $inflow->sum,
+            'partner_id' => $partner->id,
+            'cost_item_id' => $costItem->id,
+        ])->exists());
     }
 }
